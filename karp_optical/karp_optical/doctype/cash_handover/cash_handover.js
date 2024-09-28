4c897693ffeab1d142c2dcd4fb4e7d96d3b710aa
Sushhil-Pal <sushil.pal@gmail.com>
2024-09-28 08:55:21 +0530

    Added Cash Handover Doctype

---
+++ karp_optical/karp_optical/doctype/cash_handover/cash_handover.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Karp Infotech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Cash Handover", {
+// 	refresh(frm) {
+
+// 	},
+// });
