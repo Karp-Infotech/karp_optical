6cff83258a75d3cdf4a9cdd936a0b1de8aaaa21a
Sushhil-Pal <sushil.pal@gmail.com>
2024-10-14 22:01:40 +0530

    Syncing Store from SI to Payment Entry

--- karp_optical/payment_entry.py
+++ karp_optical/payment_entry.py
@@ -9,5 +9,11 @@ def copy_warehouse_from_sales_order(doc, method):
                 sales_order = frappe.get_doc("Sales Order", ref.reference_name)
 
                 doc.custom_store = sales_order.set_warehouse
+            elif ref.reference_doctype == "Sales Invoice" and ref.reference_name:
+                # Fetch the Sales Order
+                sales_invoice = frappe.get_doc("Sales Invoice", ref.reference_name)
+
+                doc.custom_store = sales_invoice.set_warehouse
+
 
 
